3d4636e72ce62ee7e88d8a7630395e0a075e1623
Alex Moise <alex@moise.pro>
2018-05-12 00:00:09 +0300

    Disable comments cookie

--- no-email-url-ip-wordpress-comments.php
+++ no-email-url-ip-wordpress-comments.php
@@ -3,8 +3,8 @@
  * Plugin Name: No Email, IP and URL for Wordpress Comments
  * Plugin URI: https://github.com/alexmoise/No-Email-IP-and-URL-for-Wordpress-Comments
  * GitHub Plugin URI: https://github.com/alexmoise/No-Email-IP-and-URL-for-Wordpress-Comments
- * Description: A very simple plugin to remove Email and Website fields from comments area and also stop collecting the commenter's IP address. No settings page needed at this moment.
- * Version: 0.1.1
+ * Description: A very simple plugin to remove Email and Website fields from comments area and also stop collecting the commenter's IP address. Also disables comment system cookie and empties comment notes text. No settings page needed at this moment, although the option to disable/enable that cookie could be nice to have in the admin area.
+ * Version: 0.1.3
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -31,5 +31,11 @@ function mo_dont_store_commenter_ip( $comment_author_ip ) {
 	return '';
 }
 add_filter( 'pre_comment_user_ip', 'mo_dont_store_commenter_ip' );
+
+// disable comments cookie
+add_action('init', 'mo_disable_comment_cookie');
+function mo_disable_comment_cookie() {
+	remove_action( 'set_comment_cookies', 'wp_set_comment_cookies' );
+}
 
 ?>
